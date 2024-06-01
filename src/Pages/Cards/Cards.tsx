fdf112029e480de4254c0cb14d34f97a27ee2453
Razvan Paros <razvanparos21@gmail.com>
2024-06-02 01:51:57 +0300

    nav

---
+++ src/Pages/Cards/Cards.tsx
@@ -0,0 +1,16 @@
+import './Cards.css';
+import React from 'react'
+import { Slide } from "react-awesome-reveal";
+
+function Cards() {
+  return (
+    <div className="cards-div padding">
+        <Slide duration={2000}>
+            cards div
+        </Slide>
+        
+    </div>
+  );
+}
+
+export default Cards;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,18 +1,25 @@
 import './Home.css';
 import React, { useEffect, useState } from 'react'
-import { Slide } from "react-awesome-reveal";
+import { Slide, Fade } from "react-awesome-reveal";
 import { FaUser } from "react-icons/fa";
 import cardImg1 from '../../Assets/Group 1000000882.png'
 import cardImg2 from '../../Assets/Group 1000000964.png'
 import cardImg3 from '../../Assets/Group 3.png'
-import sendIcon from '../../Assets/Sent-Icon.png'
-import receiveIcon from '../../Assets/Receive-Icon.png'
-import Loan from '../../Assets/Loan-Icon.png'
-import TopUp from '../../Assets/Topup.png'
+import { LiaArrowUpSolid } from "react-icons/lia";
+import { LiaArrowDownSolid } from "react-icons/lia";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { PiBank } from "react-icons/pi";
 import Add from '../../Assets/Add-Icon.png'
+import { TbHome2 } from "react-icons/tb";
+import { LuWalletCards } from "react-icons/lu";
+import { AiOutlinePieChart } from "react-icons/ai";
+import { MdOutlineSettings } from "react-icons/md";
+import Cards from '../Cards/Cards.tsx';
 
 function Home(props: any) {
     const [myCardsArray, setMyCardsArray]=useState<any[]>([]);
+    const [homeTab, setHomeTab]=useState('home');
+
 
     useEffect(()=>{
         setMyCardsArray(props.userData.myCards)
@@ -23,22 +30,23 @@ function Home(props: any) {
         props.changePage('login')
     }
     
-     
     return (
-    <Slide direction='down' duration={500}>
-        <div className="home-div padding">
-            <div className='home-top'>
-                <div className='profile-pic' onClick={signOut}>
-                    <FaUser className='user-icon'/>
+    <Slide duration={500}>
+        <div className=" home-screen">
+            {homeTab==='home'?
+                <div className="home-div padding">
+                    <div className='home-top'>
+                        <div className='profile-pic' onClick={signOut}>
+                            <FaUser className='user-icon'/>
+                        </div>
+                    <div className='home-top-name'>
+                        <p className='small-text'>Welcome back,</p>
+                        <h2>{props.userData.fullName}</h2>
+                    </div>
                 </div>
-                <div className='home-top-name'>
-                    <p className='small-text'>Welcome back,</p>
-                    <h2>{props.userData.fullName}</h2>
-                </div>
-            </div>
-            {myCardsArray?.length>0 ?(
-            <div>
-                {myCardsArray?.map((card) => (
+                {myCardsArray?.length>0 ?(
+                <div>
+                    {myCardsArray?.map((card) => (
                         <div key={card.id} className='card'>
                             <div className='card-top'>
                                 <img src={cardImg1} alt="" />
@@ -64,24 +72,58 @@ function Home(props: any) {
                             </div>
                         </div>
                     ))}
-            </div>):(<div className='add-new-card'>
-                        <p>Add new card</p>
-                        <img src={Add} alt="" />
-                    </div>)}
+                </div>):(<div className='add-new-card'>
+                            <p>Add new card</p>
+                            <img src={Add} alt="" />
+                        </div>)}
             {myCardsArray?.length>0 ?         
             <div className='card-actions'>
-                <img src={sendIcon} alt="" />
-                <img src={receiveIcon} alt="" />
-                <img src={Loan} alt="" />
-                <img src={TopUp} alt="" />
-            </div>: ''}
-            
-            <div className='transactions-div'>
-                <div className='transactions-top'>
-                    <p>Transactions</p>
-                    <p style={{color:'var(--primary-blue)'}}>See All</p>
+                <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                    <div className='action-circle'><LiaArrowUpSolid /></div>
+                    <p>Send</p>
                 </div>
-            </div>
+                <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                    <div className='action-circle'><LiaArrowDownSolid /></div>
+                    <p>Receive</p>
+                </div>
+                <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                    <div className='action-circle'><PiBank /></div>
+                    <p>Loan</p>
+                </div>
+                <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                    <div className='action-circle'><BsCurrencyDollar/></div>
+                    <p>TopUp</p>
+                </div>
+            </div>: ''}
+            {props.userData?.myCards?.transactions?.length>0?
+                <div className='transactions-div'>
+                    <div className='transactions-top'>
+                        <p>Transactions</p>
+                        <p style={{color:'var(--primary-blue)'}}>See All</p>
+                    </div>
+                </div>:<p style={{textAlign:'center'}}>No transactions</p>}
+            </div>:''}
+
+            {homeTab==='cards'?<Cards/>:''}
+
+            <nav className='nav padding'>
+                <div onClick={()=>{setHomeTab('home')}} className={`nav-button ${homeTab==='home'?'active-tab':''}`}>
+                    <TbHome2 className='icon'/>
+                    <p>Home</p>
+                </div>
+                <div onClick={()=>{setHomeTab('cards')}} className={`nav-button ${homeTab==='cards'?'active-tab':''}`}>
+                    <LuWalletCards className='icon'/>
+                    <p>Cards</p>
+                </div>
+                <div onClick={()=>{setHomeTab('statistics')}} className={`nav-button ${homeTab==='statistics'?'active-tab':''}`}>
+                    <AiOutlinePieChart className='icon'/>
+                    <p>Statistics</p>
+                </div>
+                <div onClick={()=>{setHomeTab('settings')}} className={`nav-button ${homeTab==='settings'?'active-tab':''}`}>
+                    <MdOutlineSettings className='icon'/>
+                    <p>Settings</p>
+                </div>
+            </nav>  
         </div>
     </Slide>
 );
